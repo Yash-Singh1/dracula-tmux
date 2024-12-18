e698605c8aaa54d7fa921dd506e9affa383eada4
Yash Singh <saiansh2525@gmail.com>
2024-12-17 17:35:19 -0800

    fix: example.com first for HOSTS

--- scripts/fill_line.sh
+++ scripts/fill_line.sh
@@ -4,9 +4,8 @@ current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $current_dir/utils.sh
 
 fill_line() {
-  for ((i=0; i < $(get_tmux_option "@dracula-cols" "0"); i++)); do
-    echo -n " "
-  done
+  cols=$(get_tmux_option "@dracula-cols" "0")
+  printf "%*s" $cols
   echo
 }
 
